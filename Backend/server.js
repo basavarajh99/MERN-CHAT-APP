41992f2ed1c6ab81af925ece63e4a28212005870
basavarajh99 <basavarajh7299@gmail.com>
2022-10-23 18:24:15 +0530

    Initial Backend Setup

---
+++ Backend/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const chats = require("./Data/data");
+const dotenv = require("dotenv");
+
+
+const app = express();
+dotenv.config();
+
+//home page
+app.get("/", (req, res) => {
+    res.send("API is running successfully!");
+})
+
+//to fetch all the chats
+app.get("/api/chat", (req, res) => {
+    res.send(chats);
+});
+
+//to get a specific chat
+app.get("/api/chat/:id", (req, res) => {
+    const singleChat = chats.find((c) => c._id === req.params.id);
+    res.send(singleChat);
+})
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(5000, console.log(`Server listening to PORT number ${PORT}`));
